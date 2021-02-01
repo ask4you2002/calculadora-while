fa0c77add3fa149e05204af50d65c6fb3bd0fb7a
ask4you2002 <ask4you2002@github.com>
2021-02-01 10:04:12 +0100

    1 while numeros de 5

---
+++ CalculadoraWhile.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class CalculadoraWhile here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CalculadoraWhile
+{
+
+
+    /**
+     * Constructor for objects of class CalculadoraWhile
+     */
+    public CalculadoraWhile()
+    {
+
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void multiplosDeCinco(){
+        int index = 15;
+        while (index < 95) {
+            System.out.println(index);
+            index = index + 5;
+        }
+    }
+}
